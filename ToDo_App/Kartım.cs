a2945c742d4903d05f3b95a55c8a804e86ad5387
Ayalni2289 <alininspor@hotmail.com>
2024-11-03 21:24:18 +0300

    Atanacak Kisi Dictionary olarak tanımlandı.

--- ToDo_App/Kartım.cs
+++ ToDo_App/Kartım.cs
@@ -1,10 +1,10 @@
 ﻿class Kartım{
     private string baslik;
     private string icerik;
-    private int atananKisiID;
+    private Dictionary<int,string> atananKisiID;
     private string buyukluk;
     public string Baslik { get => baslik; set => baslik = value; }
     public string Icerik { get => icerik; set => icerik = value ;}
-    public int AtananKisiID { get => atananKisiID; set => atananKisiID = value; }
+    public Dictionary<int, string> AtananKisiID { get => atananKisiID; set => atananKisiID = value; }
     public string Buyukluk { get => buyukluk; set => buyukluk = value; }
 }
