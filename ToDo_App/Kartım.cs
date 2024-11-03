09ce50aa513feb630dadf0d63a85834e9da95b37
Ayalni2289 <alininspor@hotmail.com>
2024-11-03 21:21:46 +0300

    3 adet sınıf oluşturuldu.

---
+++ ToDo_App/Kartım.cs
@@ -0,0 +1,10 @@
+﻿class Kartım{
+    private string baslik;
+    private string icerik;
+    private int atananKisiID;
+    private string buyukluk;
+    public string Baslik { get => baslik; set => baslik = value; }
+    public string Icerik { get => icerik; set => icerik = value ;}
+    public int AtananKisiID { get => atananKisiID; set => atananKisiID = value; }
+    public string Buyukluk { get => buyukluk; set => buyukluk = value; }
+}
