88b9144e263172e983c716c5e8edd12dd03c4557
Ayalni2289 <alininspor@hotmail.com>
2024-11-03 21:25:25 +0300

    Kart özelliklerinin methotları içleri boş bir şekilde tanımlandı.

--- ToDo_App/KartOzellikleri.cs
+++ ToDo_App/KartOzellikleri.cs
@@ -1,4 +1,19 @@
 class KartOzellikleri:Kartım{
     
+    public void KartEkle(){
+
+    }
+    public void KartSil(){
+
+    }
+    public void KartGuncelle(){
+
+    }
+    public void KartTasi(){
+
+    }
+    public void BoardListele(){
+
+    }
 
 }
