54f62344c03cb38c923b104b979a8a48b21fbf39
Ayalni2289 <alininspor@hotmail.com>
2024-11-09 12:02:43 +0300

    Kart silme methodu güncellendi.

--- ToDo_App/KartOzellikleri.cs
+++ ToDo_App/KartOzellikleri.cs
@@ -1,8 +1,18 @@
+using System.Linq.Expressions;
+
 class KartOzellikleri{
     
+    static Dictionary<int, TakimUyesi> takimUyeleri = new Dictionary<int, TakimUyesi>(){
+        {1, new TakimUyesi(1,"Ali")},
+        {2, new TakimUyesi(2,"Veli")},
+        {3, new TakimUyesi(3,"Ahmet")},
+        {4, new TakimUyesi(4,"Mehmet")},
+    };
     private static List<Kartım> kartlar = new List<Kartım>();
-    public static void KartEkle(){
+    public static void KartEkle()
+    {
         Kartım yeniKart = new Kartım();
+
         Console.WriteLine("Başlık Giriniz: ");
         yeniKart.Baslik = Console.ReadLine();
 
@@ -10,12 +20,34 @@ class KartOzellikleri{
         yeniKart.Icerik = Console.ReadLine();
 
         Console.WriteLine("Büyüklük Seçiniz -> XS(1),S(2),M(3),L(4),XL(5): ");
-        yeniKart.buyuklukEnum = (Kartım.BuyuklukEnum)Enum.Parse(typeof(Kartım.BuyuklukEnum), Console.ReadLine());
+        int secilenBuyukluk = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Kişi Seçiniz: ");
-        
+
+        if(Enum.IsDefined(typeof(Kartım.BuyuklukEnum), secilenBuyukluk))
+        {
+            yeniKart.Buyukluk = (Kartım.BuyuklukEnum)secilenBuyukluk;
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz Büyüklük Seçimi");
+            return;
+        }
+        Console.WriteLine("Kişi Seçiniz (ID): ");
+        int girilenID = int.Parse(Console.ReadLine());
+
+        if (takimUyeleri.ContainsKey(girilenID))
+        {
+            yeniKart.AtananKisiID = takimUyeleri[girilenID];
+            kartlar.Add(yeniKart);
+            Console.WriteLine($"Kart başarıyla eklendi: {yeniKart.Baslik} ({yeniKart.AtananKisiID.Adi})");
+        }
+        else
+        {
+            Console.WriteLine("Hatalı Giriş Yaptınız !");
+        }
     }
-    static public void KartSil(){
+    static public void KartSil()
+    {
         Console.WriteLine("Silmek istediğiniz kartın başlığını giriniz: ");
         string baslik = Console.ReadLine();
         Kartım kart = kartlar.FirstOrDefault(k => k.Baslik == baslik);
@@ -27,8 +59,29 @@ class KartOzellikleri{
         }
         else
         {
-            Console.WriteLine("Kart bulunamadı.");
+            Console.WriteLine("Aradığınız kriterlere uygun kart Board'da bulunamadı. Lütfen bir seçim yapınız.");
+            Console.WriteLine("Silmeyi Sonlandırmak için: (1)");
+            Console.WriteLine("Yeniden Denemek İçin: (2)");
+            string retry = Console.ReadLine();
+            try
+            {
+            switch (retry)
+            {
+                case "1":
+                break;
+                case "2":
+                KartSil();
+                break;
+                default:
+                    throw new Exception("Geçersiz seçim.");
+            }
         }
+        catch (Exception innerEx)
+        {
+            Console.WriteLine("Hata: "innerEx.Message);
+            KartSil();
+        }
+    }
     }
     static public void KartGuncelle(){
 
